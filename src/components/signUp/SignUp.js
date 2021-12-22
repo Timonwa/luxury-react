0135fb90891a6cd97f0a69297d903e9cfbaa5395
Pelumi Akintokun <pelumiakintokun@gmail.com>
2021-12-22 15:19:52 +0000

    added more validation to signup button and cleaned the code.

--- src/components/signUp/SignUp.js
+++ src/components/signUp/SignUp.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./signUp.scss";
-import { FaTwitter, FaFacebook, FaGoogle } from "react-icons/fa";
+import {
+  FaTwitter,
+  FaFacebook,
+  FaGoogle,
+  FaEyeSlash,
+  FaEye,
+} from "react-icons/fa";
 import { useForm } from "react-hook-form";
 
 const SignUp = () => {
+  const [viewPassword, setViewPassword] = useState(false);
+  const handlePassword = () => {
+    setViewPassword(!viewPassword);
+  };
+  const [confirmPassword, setConfirmPassword] = useState(false);
+  const handleConfirmPassword = () => {
+    setConfirmPassword(!confirmPassword);
+  };
+
   const {
     register,
     handleSubmit,
@@ -69,100 +84,130 @@ const SignUp = () => {
       <form className="signup-form" onSubmit={handleSubmit(handleSignUp)}>
         <h3>SIGN UP</h3>
         <div className="group1">
-          <label className="name-lbl" for="name" hidden></label>
-          <input
-            className="name"
-            type="text"
-            placeholder="name"
-            {...register("userName", registerOptions.name)}
-          />
+          <div className="form-input-group">
+            <label className="form-input-label" for="name" hidden></label>
+            <input
+              className="form-input"
+              type="text"
+              placeholder="name"
+              {...register("userName", registerOptions.name)}
+            />
+          </div>
           <p className="error-message">
             {errors.userName && errors.userName.message}
           </p>
-
-          <label className="email-lbl" for="email" hidden></label>
-          <input
-            className="email"
-            type="email"
-            placeholder="email"
-            {...register("userEmail", registerOptions.email)}
-          />
+          <div className="form-input-group">
+            <label className="form-input-label" for="email" hidden></label>
+            <input
+              className="form-input"
+              type="email"
+              placeholder="email"
+              {...register("userEmail", registerOptions.email)}
+            />
+          </div>
           <p className="error-message">
             {errors.userEmail && errors.userEmail.message}
           </p>
-
-          <label className="password-lbl" for="password" hidden></label>
-          <input
-            className="password"
-            type="password"
-            placeholder="password"
-            {...register("userPassword", registerOptions.password)}
-          />
+          <div className="form-input-group">
+            <label className="form-input-label" for="password" hidden></label>
+            <input
+              className="form-input"
+              type={!viewPassword ? "password" : "text"}
+              placeholder="password"
+              {...register("userPassword", registerOptions.password)}
+            />
+            {!viewPassword ? (
+              <FaEyeSlash
+                className="icon view-password"
+                onClick={handlePassword}
+              />
+            ) : (
+              <FaEye className="icon view-password" onClick={handlePassword} />
+            )}
+          </div>
           <p className="error-message">
             {errors.userPassword && errors.userPassword.message}
           </p>
-
-          <label
-            className="confirm-password-lbl"
-            for="confirm-password"
-            hidden></label>
-          <input
-            className="confirm-password"
-            type="password"
-            placeholder="confirm password"
-            {...register("confirmPassword", registerOptions.confirmPassword)}
-          />
+          <div className="form-input-group">
+            <label
+              className="form-input-label"
+              for="confirm-password"
+              hidden></label>
+            <input
+              className="form-input"
+              type={!confirmPassword ? "password" : "text"}
+              placeholder="confirm password"
+              {...register("confirmPassword", registerOptions.confirmPassword)}
+            />
+            {!confirmPassword ? (
+              <FaEyeSlash
+                className="icon view-password"
+                onClick={handleConfirmPassword}
+              />
+            ) : (
+              <FaEye
+                className="icon view-password"
+                onClick={handleConfirmPassword}
+              />
+            )}
+          </div>
           <p className="error-message">
             {errors.confirmPassword && errors.confirmPassword.message}
           </p>
 
-          <button className="signup-btn" type="submit">
-            Sign Up
-          </button>
+          <div className="form-input-group">
+            <button className="signup-btn" type="submit">
+              Sign Up
+            </button>
+          </div>
         </div>
 
         <div className="group2-signup">
           {/* terms and conditions checkbox */}
-          <div className="terms">
-            <input
-              className="terms"
-              type="checkbox"
-              {...register("acceptTerms", registerOptions.acceptTerms)}
-            />
-            <label for="acceptTerms">
-              I accept the{" "}
-              <Link className="terms-cons" to="">
-                terms and conditions
-              </Link>{" "}
-              and{" "}
-              <Link className="privacy-policy" to="">
-                privacy policy
-              </Link>
-            </label>
+          <div className="form-input-group">
+            <div className="terms">
+              <input
+                className="terms"
+                type="checkbox"
+                {...register("acceptTerms", registerOptions.acceptTerms)}
+              />
+              <label for="acceptTerms">
+                I accept the{" "}
+                <Link className="terms-cons" to="">
+                  terms and conditions
+                </Link>{" "}
+                and{" "}
+                <Link className="privacy-policy" to="">
+                  privacy policy
+                </Link>
+              </label>
+            </div>
+            <p className="error-message">
+              {errors.acceptTerms && errors.acceptTerms.message}
+            </p>
           </div>
-          <p className="error-message">
-            {errors.acceptTerms && errors.acceptTerms.message}
-          </p>
 
-          {/* <!-- radio buttons services --> */}
-          <div className="our-services">
-            <span className="">I am: </span>
-            <input
-              className="signIn-radio"
-              type="radio"
-              value="looking"
-              checked
-              {...register("userService")}
-            />
-            <label className="">looking for accommodation.</label>
+          <div className="form-input-group">
+            {/* <!-- radio buttons services --> */}
+            <div className="our-services">
+              <span className="">I am: </span>
+              <input
+                className="signIn-radio"
+                type="radio"
+                value="looking"
+                checked
+                {...register("userService")}
+              />
+              <label className="">looking for accommodation.</label>
 
-            <input
-              className="signIn-radio"
-              type="radio"
-              value="offering"
-              {...register("userService")}
-            />
-            <label className="">offering accommodation.</label>
+              <input
+                className="signIn-radio"
+                type="radio"
+                value="offering"
+                {...register("userService")}
+              />
+              <label className="">offering accommodation.</label>
+            </div>
           </div>
         </div>
 
