0f573c39a4ef28d3b9904b961ce9e0b8bf596577
Pelumi Akintokun <pelumiakintokun@gmail.com>
2021-12-29 00:32:42 +0000

    added listings page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Route, Routes } from "react-router";
 import Homepage from "./pages/Homepage/Homepage";
+import Listings from "./pages/ListingsPage/ListingsPage";
 import LoginPage from "./pages/AuthPages/LoginPage/LoginPage";
 import SignUpPage from "./pages/AuthPages/SignUpPage/SignUpPage";
 import ForgotPasswordPage from "./pages/AuthPages/ForgotPasswordPage/ForgotPasswordPage";
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/listings" element={<Listings />} />
       </Routes>
     </div>
   );
