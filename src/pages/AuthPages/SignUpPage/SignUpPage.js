2830a05ffb55a938c3596f6f36f67a32c25c852c
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-01-24 15:44:00 +0100

    Imported useEffect

--- src/pages/AuthPages/SignUpPage/SignUpPage.js
+++ src/pages/AuthPages/SignUpPage/SignUpPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Footer from "../../../components/Footer/Footer";
 import SignUp from "../../../components/forms/SignUp/SignUp";
 import NavBar from "../../../components/NavBar/NavBar"
