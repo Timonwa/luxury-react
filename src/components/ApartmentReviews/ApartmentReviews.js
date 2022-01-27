5761b1279f1fecf32d9aa4a18462cc11bd3f4a71
Pelumi Akintokun <pelumiakintokun@gmail.com>
2022-01-27 05:31:17 +0000

    made user ad review more responsive.

--- src/components/ApartmentReviews/ApartmentReviews.js
+++ src/components/ApartmentReviews/ApartmentReviews.js
@@ -36,62 +36,8 @@ const ApartmentReviews = () => {
               </p>
             </div>
           </div>
-          
-          <div className="user-ad-review">
-            <div className="user-ad-review-heading">
-              {/* <!-- user star ratings --> */}
-              <RatingsBadge />
-              <div className="user-name-date">
-                {/* <!-- user name --> */}
-                <h3 className="user-ad-name">User1</h3>
-                {/* <!-- date the post was made --> */}
-                <p className="user-ad-review-date">
-                  <small> January 6th, 2021</small>
-                </p>
-              </div>
-            </div>
-            {/* <!-- user review --> */}
-            <div className="user-ad-review-body">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
-                ipsam a sed aliquid. Quidem quam explicabo excepturi! Tempore
-                esse doloribus quaerat. Odit perferendis quidem asperiores
-                molestiae. Odio amet et iste.Est inventore dicta, iste nobis
-                possimus cupiditate quasi saepe corrupti vitae velit natus
-                necessitatibus architecto libero a ea voluptas, officiis, ipsa
-                soluta optio quia molestias dolor cumque incidunt. Nesciunt,
-                laborum?
-              </p>
-            </div>
-          </div>
-          <div className="user-ad-review">
-            <div className="user-ad-review-heading">
-              {/* <!-- user star ratings --> */}
-              <RatingsBadge />
-              <div className="user-name-date">
-                {/* <!-- user name --> */}
-                <h3 className="user-ad-name">User1</h3>
-                {/* <!-- date the post was made --> */}
-                <p className="user-ad-review-date">
-                  <small> January 6th, 2021</small>
-                </p>
-              </div>
-            </div>
-            {/* <!-- user review --> */}
-            <div className="user-ad-review-body">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
-                ipsam a sed aliquid. Quidem quam explicabo excepturi! Tempore
-                esse doloribus quaerat. Odit perferendis quidem asperiores
-                molestiae. Odio amet et iste.Est inventore dicta, iste nobis
-                possimus cupiditate quasi saepe corrupti vitae velit natus
-                necessitatibus architecto libero a ea voluptas, officiis, ipsa
-                soluta optio quia molestias dolor cumque incidunt. Nesciunt,
-                laborum?
-              </p>
-            </div>
-          </div>
         </div>
+        <p className="load-more-btn">view more...</p>
       </div>
     </section>
   );
