c0ec5cb81da8b12442f39abd07fd4d6170a041ed
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2023-01-05 15:04:16 +0100

    cleaned code.

--- src/components/FeaturedPosts/FeaturedPosts.js
+++ src/components/FeaturedPosts/FeaturedPosts.js
@@ -1,16 +1,12 @@
 import React from "react";
 import "./FeaturedPosts.scss";
 import PostCardPreview from "../PostCardPreview/PostCardPreview";
-// import useFetch from "../hooks/useFetch/useFetch";
 
-const FeaturedPosts = ({ posts, isPending, error }) => {
+const FeaturedPosts = ({ posts }) => {
   return (
     <main className="feature-posts-section">
       <div className="feature-posts-section-wrapper">
         <h3 className="section-title">Posts</h3>
-        {/* <!-- the ads --> */}
-        {/* {error && <div className="error">{error}</div>}
-        {isPending && <h3 className="loading">Loading...</h3>} */}
         <div className="feature-posts-cntr">
           {posts && <PostCardPreview posts={posts} />}
         </div>
--- src/pages/ListingsPage/ListingsPage.js
+++ src/pages/ListingsPage/ListingsPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Footer from "../../components/Footer/Footer";
 import NavBar from "../../components/NavBar/NavBar";
 import SearchCard from "../../components/SearchCard/SearchCard";
@@ -6,22 +6,19 @@ import Listings from "../../components/Listings/Listings";
 import RegisterCTA from "../../components/RegisterCTA/RegisterCTA";
 import "./ListingsPage.scss";
 import ChatBubble from "../../components/ChatBubble/ChatBubble";
+import { posts } from "../../database/db";
 
 const ListingsPage = () => {
-  useEffect(() => {
-    window.scroll(0,0)
-  }, [])
-
   return (
     <div className="listings-page">
       <NavBar />
       <header className="header-section">
         <div className="header-wrapper">
           <div className="header-space"></div>
-          <SearchCard />
+          <SearchCard posts={posts} />
         </div>
       </header>
-      <Listings />
+      <Listings posts={posts} />
       <RegisterCTA />
       <Footer />
       <ChatBubble />
